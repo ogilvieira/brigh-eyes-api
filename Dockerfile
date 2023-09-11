e79ab8e4fae1eba6698d4f125e36a00e50042ff9
Gil Vieira <ogilvieira@gmail.com>
2023-09-11 14:33:06 -0300

    set docker compose

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN npm cache clean --force
 RUN npm install
 COPY . .
 EXPOSE 8090
-CMD [ "npm", "run", "start" ]
+CMD [ "npm", "run", "dev" ]
--- app/index.ts
+++ app/index.ts
@@ -5,7 +5,14 @@ import createServer from './server';
 (async () => {
     try {
         const server = await createServer();
-        server.listen({ port: +(process.env.PORT ?? 80)});
+        server.listen({ host: '0.0.0.0', port: +(process.env.PORT ?? 80)}, (err, address) => {
+          console.info('listen on %s', address)
+          if(err) {
+            server.log.error(err);
+            process.exit(1)
+          }
+        });
+
     } catch (err) {
         console.error(err);
     }
